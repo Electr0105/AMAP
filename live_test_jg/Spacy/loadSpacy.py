b2fe90149bbbfe67d371074c6814997e5abb0c3f
JacksonGleeson <jackson.gleeson@gmail.com>
2022-08-09 12:35:08 +1000

    Spacy now saves output to a txt

--- live_test_jg/Spacy/loadSpacy.py
+++ live_test_jg/Spacy/loadSpacy.py
@@ -2,10 +2,14 @@ import spacy
 
 def spacy_run(value):
 	"""Basic run function for spacy, load the saved model and predict"""
+	open_file = open('Spacy/spacy_output.txt','w', encoding="utf-8")
 	output_dir = "Spacy/amapNER"
 	nlp_load = spacy.load(output_dir)
 	doc = nlp_load(value)
 	store = []
 	for ent in doc.ents:
-		store.append(str(ent.label_) + ": " + str(ent))
+		label_and_value = "{} : {} \n".format(str(ent.label_), str(ent))
+		store.append(label_and_value)
+		open_file.write(label_and_value)
+	open_file.close()
 	return store
