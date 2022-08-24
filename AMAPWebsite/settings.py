8abf7776c1d9a693c1564a15bee75d1384e684b6
JacksonGleeson <jackson.gleeson@gmail.com>
2022-08-24 11:45:19 +1000

    Update settings.py

--- AMAPWebsite/settings.py
+++ AMAPWebsite/settings.py
@@ -18,6 +18,9 @@ STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
+    BASE_DIR / "static/assets",
+    BASE_DIR / "static/css",
+    BASE_DIR / "static/logo",
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media/')
