b4b42c09d16420fb27503c1244e4672269cbd2bc
JacksonGleeson <jackson.gleeson@gmail.com>
2022-08-24 12:03:33 +1000

    static

--- AMAPWebsite/settings.py
+++ AMAPWebsite/settings.py
@@ -15,10 +15,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/website/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "website/static"),
+    BASE_DIR / "static",
+    '/var/www/static/',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media/')
