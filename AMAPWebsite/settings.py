fa00341ecafc48cc0110390d09b8bae382b374d5
JacksonGleeson <jackson.gleeson@gmail.com>
2022-08-24 12:12:59 +1000

    Update settings.py

--- AMAPWebsite/settings.py
+++ AMAPWebsite/settings.py
@@ -13,7 +13,7 @@ import mimetypes
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 mimetypes.add_type("text/css", ".css", True)
